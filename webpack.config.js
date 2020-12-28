ff99dcb6ab759d34d6e94a90d318c34a2a9b25e6
abdigun <zzz.azahan@yahoo.com>
2020-12-28 18:52:16 +0800

    add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,44 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+
+	 module: {
+	 rules: [
+	 	// css loader 
+	   {
+	     test: /\.css$/,
+	     use: [
+	       'style-loader',
+	       'css-loader',
+	     ],
+	   },
+	   // file loader
+       {
+         test: /\.(png|svg|jpg|gif)$/,
+         use: [
+           'file-loader',
+         ],
+       },
+       //  font loader
+       {
+         test: /\.(woff|woff2|eot|ttf|otf)$/,
+         use: [
+           'file-loader',
+         ],
+       },
+       //  loader data
+       {
+         test: /\.(csv|tsv)$/,
+         use: [
+           'csv-loader',
+         ],
+       },
+       {
+         test: /\.xml$/,
+         use: [
+           'xml-loader',
+         ],
+       },
+	 ],
+	},
 };
